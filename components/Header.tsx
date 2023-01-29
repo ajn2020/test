fb93aa8b22c9237eb32e7a44713eadce4fc4d232
Meezeus <kac3131@gmail.com>
2023-01-29 20:59:00 +0000

    Add a header and a footer

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <>
+            <div className='layout-header'>
+                <div className='layout-header-title'>Hounslow Recycling Hub</div>
+                <br />
+                <div className='layout-header-description'>Your go-to stop for all recycling things in the London Borough of Hounslow!</div>
+            </div>
+        </>
+    )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
+import '@/styles/layout.css'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
 }
