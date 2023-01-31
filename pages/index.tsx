b0446755db5df51aebe3451c8c5c02f1bb43ccde
Meezeus <kac3131@gmail.com>
2023-01-31 00:02:38 +0000

    Create first draft of accordions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import RecyclingServiceAccordion from '@/components/RecyclingServiceAccordion'
 import Head from 'next/head'
 import Image from 'next/image'
 
@@ -11,7 +12,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-      
+        <RecyclingServiceAccordion />
+        <RecyclingServiceAccordion />
+        <RecyclingServiceAccordion />
       </main>
     </>
   )
