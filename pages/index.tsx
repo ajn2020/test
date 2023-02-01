3fd3b88b9785ab375a12d6eed84927f0d9f05a97
Meezeus <kac3131@gmail.com>
2023-02-01 20:33:29 +0000

    Add subheadings

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import AccordionGrid from '@/components/AccordionGrid'
+import Subheading from '@/components/Subheading'
 import Head from 'next/head'
 
 export default function Home() {
@@ -11,6 +12,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Subheading
+        title="Recycling Services Available"
+      />
       <AccordionGrid />
     </>
   )
