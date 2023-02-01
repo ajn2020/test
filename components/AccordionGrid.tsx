91688fe5524538a2ec1975e67adb909be0e9bfbb
Meezeus <kac3131@gmail.com>
2023-02-01 00:03:42 +0000

    Add another row of accordions

--- components/AccordionGrid.tsx
+++ components/AccordionGrid.tsx
@@ -31,6 +31,20 @@ export default function AccordionGrid() {
           content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et pretium orci. Aliquam nec maximus felis, nec fringilla augue. In turpis lorem, tincidunt eget mi non, lobortis condimentum turpis. Pellentesque fermentum lorem et enim vulputate varius. Nam sit amet convallis orci. Etiam lorem turpis, dignissim id sapien at, sodales finibus arcu. Curabitur auctor vehicula turpis, ac mollis neque maximus eu. Integer blandit placerat pellentesque. Nullam auctor lorem nec ante finibus venenatis. Morbi quis felis velit. Curabitur id felis ultrices, auctor mi in, viverra enim. Quisque iaculis tellus quis velit blandit consequat. Quisque in congue neque, eget lacinia elit. Cras malesuada maximus varius. Curabitur eget maximus nibh. Ut a eleifend velit, sed tempor est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec vel sem sagittis, volutpat elit sit amet, malesuada erat."
         />
       </div>
+      <div className='accordion-grid-item'>
+      <RecyclingServiceAccordion
+          title="Lorem ipsum"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et pretium orci. Aliquam nec maximus felis, nec fringilla augue. In turpis lorem, tincidunt eget mi non, lobortis condimentum turpis. Pellentesque fermentum lorem et enim vulputate varius. Nam sit amet convallis orci. Etiam lorem turpis, dignissim id sapien at, sodales finibus arcu. Curabitur auctor vehicula turpis, ac mollis neque maximus eu. Integer blandit placerat pellentesque. Nullam auctor lorem nec ante finibus venenatis. Morbi quis felis velit. Curabitur id felis ultrices, auctor mi in, viverra enim. Quisque iaculis tellus quis velit blandit consequat. Quisque in congue neque, eget lacinia elit. Cras malesuada maximus varius. Curabitur eget maximus nibh. Ut a eleifend velit, sed tempor est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec vel sem sagittis, volutpat elit sit amet, malesuada erat."
+        />
+      </div>
+      <div className='accordion-grid-item'>
+      <RecyclingServiceAccordion
+          title="Lorem ipsum"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et pretium orci. Aliquam nec maximus felis, nec fringilla augue. In turpis lorem, tincidunt eget mi non, lobortis condimentum turpis. Pellentesque fermentum lorem et enim vulputate varius. Nam sit amet convallis orci. Etiam lorem turpis, dignissim id sapien at, sodales finibus arcu. Curabitur auctor vehicula turpis, ac mollis neque maximus eu. Integer blandit placerat pellentesque. Nullam auctor lorem nec ante finibus venenatis. Morbi quis felis velit. Curabitur id felis ultrices, auctor mi in, viverra enim. Quisque iaculis tellus quis velit blandit consequat. Quisque in congue neque, eget lacinia elit. Cras malesuada maximus varius. Curabitur eget maximus nibh. Ut a eleifend velit, sed tempor est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec vel sem sagittis, volutpat elit sit amet, malesuada erat."
+        />
+      </div>
     </div>
   )
 }
