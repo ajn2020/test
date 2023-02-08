34053d3831833c79de1783a66435e7dfc17a5961
jianaiasus <k1897735@kcl.ac.uk>
2023-02-08 20:27:36 +0000

    version2

---
+++ data/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <>
+            <div className ='footer'>
+                <p>Copyright &copy; 2023 Hounslow Recycling Hub</p>
+            </div>
+        </>
+    )
+}
---
+++ data/pages/index.tsx
@@ -0,0 +1,23 @@
+import AccordionGrid from '@/components/AccordionGrid'
+import Subheading from '@/components/Subheading'
+import DecisionTree from '@/components/DecisionTree'
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Hounslow Recycling Hub</title>
+        <meta name="application-name" content="Hounslow Recycling Hub" />
+        <meta name="description" content="Your go-to stop for all recycling things in the London Borough of Hounslow!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <DecisionTree />
+
+      <Subheading title="Recycling Services Available" />
+      <AccordionGrid />
+    </>
+  )
+}
