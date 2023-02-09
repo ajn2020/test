535ea32f6e10627819e1e9bbdb0494ff7f11a5d1
Meezeus <kac3131@gmail.com>
2023-02-09 17:02:27 +0000

    Add functionality to card carousel

--- components/EventCardCarousel.tsx
+++ components/EventCardCarousel.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import EventCard from "./EventCard";
 import { EventCardProps } from "./EventCard";
 import { v4 as uuidv4 } from 'uuid';
@@ -7,16 +8,38 @@ type EventCardCarouselProps = {
 }
 
 export default function EventCardCarousel(props: EventCardCarouselProps) {
+  const [isDisabledPrevious, setDisabledPrevious] = useState(true)
+  const [isDisabledNext, setDisabledNext] = useState(false)
+  const [range, setRange] = useState([0, 3]);
+  const displayedEvents = props.events.slice(...range);
+
+  useEffect(() => {
+    if (range[0] == 0) {
+      setDisabledPrevious(true)
+    }
+    else {
+      setDisabledPrevious(false)
+    }
+    if (range[1] >= props.events.length) {
+      setDisabledNext(true)
+    }
+    else {
+      setDisabledNext(false)
+    }
+  }, [range])
+
   return (
-    <div className='event-card-carousel'>
+    <div className='event-card-carousel-container'>
       <div className="event-card-carousel-button-div">
-        <button className="event-card-carousel-button" type="button">Previous</button>
+        <button className="event-card-carousel-button" type="button" disabled={isDisabledPrevious} onClick={() => setRange(range.map(x => x-3))}>Previous</button>
       </div>
-      {props.events.map(event => (
+      <div className='event-card-carousel'>
+        {displayedEvents.map(event => (
           <EventCard key={uuidv4()} image={event.image} title={event.title} date={event.date} description={event.description} />
-      ))}
+        ))}
+      </div>
       <div className="event-card-carousel-button-div">
-        <button className="event-card-carousel-button" type="button">Next</button>
+        <button className="event-card-carousel-button" type="button" disabled={isDisabledNext} onClick={() => setRange(range.map(x => x+3))}>Next</button>
       </div>
     </div>
   )
--- data/Events.tsx
+++ data/Events.tsx
@@ -16,5 +16,35 @@ export default [
         title: "Event 3",
         date: "dd/mm/yyyy",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et pretium orci. Aliquam nec maximus felis, nec fringilla augue. In turpis lorem, tincidunt eget mi non, lobortis condimentum turpis. Pellentesque fermentum lorem et enim vulputate varius. Nam sit amet convallis orci. Etiam lorem turpis, dignissim id sapien at, sodales finibus arcu. Curabitur auctor vehicula turpis, ac mollis neque maximus eu."
-    }
+    },
+    {
+        image: "/favicon.ico",
+        title: "Event 4",
+        date: "dd/mm/yyyy",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et pretium orci. Aliquam nec maximus felis, nec fringilla augue. In turpis lorem, tincidunt eget mi non, lobortis condimentum turpis. Pellentesque fermentum lorem et enim vulputate varius. Nam sit amet convallis orci. Etiam lorem turpis, dignissim id sapien at, sodales finibus arcu. Curabitur auctor vehicula turpis, ac mollis neque maximus eu."
+    },
+    {
+        image: "/favicon.ico",
+        title: "Event 5",
+        date: "dd/mm/yyyy",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et pretium orci. Aliquam nec maximus felis, nec fringilla augue. In turpis lorem, tincidunt eget mi non, lobortis condimentum turpis. Pellentesque fermentum lorem et enim vulputate varius. Nam sit amet convallis orci. Etiam lorem turpis, dignissim id sapien at, sodales finibus arcu. Curabitur auctor vehicula turpis, ac mollis neque maximus eu."
+    },
+    {
+        image: "/favicon.ico",
+        title: "Event 6",
+        date: "dd/mm/yyyy",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et pretium orci. Aliquam nec maximus felis, nec fringilla augue. In turpis lorem, tincidunt eget mi non, lobortis condimentum turpis. Pellentesque fermentum lorem et enim vulputate varius. Nam sit amet convallis orci. Etiam lorem turpis, dignissim id sapien at, sodales finibus arcu. Curabitur auctor vehicula turpis, ac mollis neque maximus eu."
+    },
+    {
+        image: "/favicon.ico",
+        title: "Event 7",
+        date: "dd/mm/yyyy",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et pretium orci. Aliquam nec maximus felis, nec fringilla augue. In turpis lorem, tincidunt eget mi non, lobortis condimentum turpis. Pellentesque fermentum lorem et enim vulputate varius. Nam sit amet convallis orci. Etiam lorem turpis, dignissim id sapien at, sodales finibus arcu. Curabitur auctor vehicula turpis, ac mollis neque maximus eu."
+    },
+    {
+        image: "/favicon.ico",
+        title: "Event 8",
+        date: "dd/mm/yyyy",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et pretium orci. Aliquam nec maximus felis, nec fringilla augue. In turpis lorem, tincidunt eget mi non, lobortis condimentum turpis. Pellentesque fermentum lorem et enim vulputate varius. Nam sit amet convallis orci. Etiam lorem turpis, dignissim id sapien at, sodales finibus arcu. Curabitur auctor vehicula turpis, ac mollis neque maximus eu."
+    },
 ]
