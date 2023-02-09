39cfcee20cfcf8df331a032b1aa52ef2d75dabcd
Meezeus <kac3131@gmail.com>
2023-02-09 14:07:17 +0000

    Add uuid

--- components/EventCardCarousel.tsx
+++ components/EventCardCarousel.tsx
@@ -1,5 +1,6 @@
 import EventCard from "./EventCard";
 import { EventCardProps } from "./EventCard";
+import { v4 as uuidv4 } from 'uuid';
 
 type EventCardCarouselProps = {
   events: Array<EventCardProps>
@@ -12,7 +13,7 @@ export default function EventCardCarousel(props: EventCardCarouselProps) {
         <button className="event-card-carousel-button" type="button">Previous</button>
       </div>
       {props.events.map(event => (
-          <EventCard image={event.image} title={event.title} date={event.date} description={event.description} />
+          <EventCard key={uuidv4()} image={event.image} title={event.title} date={event.date} description={event.description} />
       ))}
       <div className="event-card-carousel-button-div">
         <button className="event-card-carousel-button" type="button">Next</button>
