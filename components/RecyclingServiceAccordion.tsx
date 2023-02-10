f9df7c3a9a9005a4e004d2cdf25fe5511f3e8f18
Meezeus <kac3131@gmail.com>
2023-02-10 21:27:47 +0000

    Rework accordions

--- components/RecyclingServiceAccordion.tsx
+++ components/RecyclingServiceAccordion.tsx
@@ -1,41 +1,26 @@
-import { imageConfigDefault } from 'next/dist/shared/lib/image-config';
-import { useState } from 'react'
-import picDress from "./pictureAddress";
+import React, { Ref} from "react";
+
 type RecyclingServiceProps = {
+    id: string;
     title: string;
     description: string;
     content: string;
+    isOpen: boolean;
+    handleClick: (id: string) => void;
 }
 
-export default function RecyclingServiceAccordion(props: RecyclingServiceProps) {
-const {PictureAddress,topAddress}=picDress;
-    const [isOpen, setOpen] = useState(false);
-    const [accordionButton,SetAccordionButton]=useState('accordion-button')
-    const handleClick = () => {
-        setOpen(!isOpen);
-        if(accordionButton==="accordion-button"){
-            SetAccordionButton("accordion-button accordion-button-active");
-        }else{
-            SetAccordionButton("accordion-button");
-        }
-    }
-
-    return (
-        <div className="accordion-wrapper" onClick={handleClick}>
-            <div className="accordion">
-                <div className="accordion-title">
-                    <h2>{props.title}</h2>
-                </div>
-                <div className="accordion-description">
-                    <p>{props.description}</p>
-                </div>
-                <div className={isOpen? "accordion-content-displayed" : "accordion-content-hidden"}>                    
-                    {/* <br /> */}
-                    <p>{props.content}</p>
-                </div>
-            </div>  
-            {/* "Expand"       */}
-            <button className={accordionButton} type="button" >{isOpen? <img src={topAddress}/>: "click anywhere to expand"}</button>
+export default React.forwardRef((props: RecyclingServiceProps, ref: Ref<HTMLDivElement>) => (
+    <div ref={ref} id={props.id} className="accordion-wrapper" onClick={() => props.handleClick(props.id)}>
+        <div className="accordion">
+            <div className="accordion-title">
+                <h2>{props.title}</h2>
+            </div>
+            <div className="accordion-description">
+                <p>{props.description}</p>
+            </div>
+            <div className={props.isOpen ? "accordion-content-displayed" : "accordion-content-hidden"}>
+                <p>{props.content}</p>
+            </div>
         </div>
-    )
-}
+    </div>
+))
