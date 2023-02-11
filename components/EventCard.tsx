dc91ad1305bb6d984ccec9923cd7f66d453a8b5f
Meezeus <kac3131@gmail.com>
2023-02-11 19:02:44 +0000

    Rework event cards to be flippable

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,3 +1,6 @@
+import ReactMarkdown from 'react-markdown'
+import { useState } from 'react'
+
 export type EventCardProps = {
     image: string;
     title: string;
@@ -6,16 +9,23 @@ export type EventCardProps = {
 }
 
 export default function EventCard(props: EventCardProps) {
+    const [isFlipped, setFlipped] = useState(false);
+
     return (
-        <div className="event-card">
-            <div className="event-card-image-div">
-                <img src={props.image} />
-            </div>
-            <div className="event-card-content">
-                <h2>{props.title}</h2>
-                <h3>{props.date}</h3>
-                <br />
-                <p>{props.description}</p>
+        <div className="event-card-container" onClick={() => setFlipped(!isFlipped)}>
+            <div className={`event-card ${isFlipped ? 'event-card-flipped' : ''}`}>
+                <div className="event-card-front">
+                    <div>
+                        <img className="event-card-image" src={props.image} />
+                    </div>
+                    <div>
+                        <h2>{props.title}</h2>
+                        <h3>{props.date}</h3>
+                    </div>
+                </div>
+                <div className="event-card-back">
+                    <ReactMarkdown>{props.description}</ReactMarkdown>
+                </div>
             </div>
         </div>
     )
