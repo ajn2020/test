56c077eb1f7ed064007c1cc169080b60c3a85fbb
Meezeus <kac3131@gmail.com>
2023-02-12 21:36:04 +0000

    Remove unused import

--- components/RecyclingServiceAccordionGrid.tsx
+++ components/RecyclingServiceAccordionGrid.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect, createRef } from 'react'
-import { v4 as uuidv4 } from 'uuid';
 import RecyclingServiceAccordion from '@/components/RecyclingServiceAccordion'
 import { RecyclingServices } from '@/data/RecyclingServices';
 
