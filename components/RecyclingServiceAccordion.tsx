4c7656ef8232b72b093e61172ee50a7005d32773
Meezeus <kac3131@gmail.com>
2023-02-12 19:53:03 +0000

    Fix linter error

--- components/RecyclingServiceAccordion.tsx
+++ components/RecyclingServiceAccordion.tsx
@@ -1,4 +1,4 @@
-import React, { useState, Ref } from "react";
+import React, { Ref } from "react";
 import ReactMarkdown from 'react-markdown'
 
 export type RecyclingServiceAccordionProps = {
@@ -10,19 +10,21 @@ export type RecyclingServiceAccordionProps = {
     handleClick: (id: string) => void;
 }
 
-export default React.forwardRef((props: RecyclingServiceAccordionProps, ref: Ref<HTMLDivElement>) => (
-    <div ref={ref} id={props.id} className="recycling-service-accordion-wrapper" onClick={() => props.handleClick(props.id)}>
-        <div className="recycling-service-accordion">
-            <div className="recycling-service-accordion-title">
-                <h2>{props.title}</h2>
-            </div>
-            <div className="recycling-service-accordion-description">
-                <p>{props.description}</p>
-            </div>
-            <div className={props.isOpen ? "recycling-service-accordion-content-displayed" : "recycling-service-accordion-content-hidden"}>
-                <br />
-                <ReactMarkdown>{props.content}</ReactMarkdown>
+export default React.forwardRef<HTMLDivElement, RecyclingServiceAccordionProps>(function RecyclingServiceAccordion(props: RecyclingServiceAccordionProps, ref: Ref<HTMLDivElement>) {
+    return (
+        <div ref={ref} id={props.id} className="recycling-service-accordion-wrapper" onClick={() => props.handleClick(props.id)}>
+            <div className="recycling-service-accordion">
+                <div className="recycling-service-accordion-title">
+                    <h2>{props.title}</h2>
+                </div>
+                <div className="recycling-service-accordion-description">
+                    <p>{props.description}</p>
+                </div>
+                <div className={props.isOpen ? "recycling-service-accordion-content-displayed" : "recycling-service-accordion-content-hidden"}>
+                    <br />
+                    <ReactMarkdown>{props.content}</ReactMarkdown>
+                </div>
             </div>
         </div>
-    </div>
-))
+    )
+});
