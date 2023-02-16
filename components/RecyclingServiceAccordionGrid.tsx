e88406a4b8bc40d6dc44541878d810f23bb82529
Meezeus <kac3131@gmail.com>
2023-02-16 01:32:18 +0000

    Add comment explaining bug

--- components/RecyclingServiceAccordionGrid.tsx
+++ components/RecyclingServiceAccordionGrid.tsx
@@ -113,3 +113,25 @@ export default function RecyclingServiceAccordionGrid(
     </div>
   );
 }
+
+// There is a known bug with the approach used to keep the accordions the same
+// height as the tallest one. To recreate this bug, start off with the browser
+// window being the maximum width and then open the tallest accordion (the one
+// that sets the height for all the other accordions). Next, resize the window
+// to be a significantly smaller width and then close the accordion. The closed
+// accordion will now be taller than all the other accordions.
+//
+// This is because when you resized the window and the height of the tallest
+// accordion was calculated, the opened accordion was ignored (after all we care
+// about the height of the tallest closed accordion). So the height calculated
+// was the height of the second tallest accordion, which by definition is less
+// than the height of the tallest accordion. So when the tallest accordion is
+// closed, the other accordions will be at a smaller height than it is.
+//
+// While I was able to solve this bug (add openAccordionID to useEffect
+// dependencies, keep track of previous openAccordionID using useRef, add a
+// setTimeout to the useEffect body with the time being 0 if the previous
+// openAccordionID is "" and 200 otherwise, clear timeout in the callback
+// function), the solution produced unintended side-effects which I deemed to be
+// less desirable than the bug itself. As I was unable to produce a solution
+// without these side-effects, the bug remains.
