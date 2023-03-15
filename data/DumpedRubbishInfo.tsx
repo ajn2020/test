35c8099b02fc93821678e966ffed69051abee3e0
Meezeus <kac3131@gmail.com>
2023-03-15 22:49:16 +0000

    Fix (kinda) markdown

--- data/DumpedRubbishInfo.tsx
+++ data/DumpedRubbishInfo.tsx
@@ -7,21 +7,20 @@ export type DumpedRubbishInfo = {
 
 export const dumpedRubbishInfo = {
   content:
-    "# Dumped Rubbish  \n" +
+    "# Dumped Rubbish  \n  \n" +
     "Dumping rubbish is considered an **illegal** activity. " +
     "Reporting the activity immediately will help us in our fight against this illegal activity. " +
-    "All the information your report is important and will be treated confidentially.  \n" +
-    "&nbsp;  \n" +
-    "&nbsp;  \n" +
-    "## What to include in the report  \n" +
+    "All the information your report is important and will be treated confidentially.  \n  \n" +
+    "&nbsp;  \n  \n" +
+    "## What to include in the report  \n  \n" +
     "Please, add to your report any information about the dumped rubbish, including:  \n" +
     "* location  \n" +
     "* description  \n" +
     "* photos  \n" +
     "* date and time  \n" +
     "* description of the fly-tippers (if available)  \n" +
-    "* description of the vehicle involved (if any)  \n" +
-    "&nbsp;  \n" +
+    "* description of the vehicle involved (if any)  \n  \n" +
+    "&nbsp;  \n  \n" +
     "**Note:** Do not approach any individuals and do not put yourself in any danger!",
   reportPublicForm: "https://hounslowhighways.org/",
   reportPrivateForm:
