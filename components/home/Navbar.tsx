83ca2597ddf4a6c3a02d4e8ddc8457334c1461c1
Meezeus <kac3131@gmail.com>
2023-03-19 22:29:54 +0000

    Move navbar css to module

--- components/home/Navbar.tsx
+++ components/home/Navbar.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import style from "@/styles/home/Navbar.module.css";
 
 export default function Navbar() {
   const [navList] = useState([
@@ -18,17 +19,25 @@ export default function Navbar() {
 
   return (
     <div>
-      <div className="navbar animate__animated animate__fadeIn">
-        <div className="navbarCenter">
-          <div className="navbarCenterLeft">
-            <div className="navbarCenterLogoBox">
-              <img className="navbarCenterLogo" src="/logo.svg" />
-              <div className="navbarCenterLogoName">Hounslow Recycling Hub</div>
+      <div
+        className={style["navbar"] + " " + "animate__animated animate__fadeIn"}
+      >
+        <div className={style["navbarCenter"]}>
+          <div className={style["navbarCenterLeft"]}>
+            <div className={style["navbarCenterLogoBox"]}>
+              <img className={style["navbarCenterLogo"]} src="/logo.svg" />
+              <div className={style["navbarCenterLogoName"]}>
+                Hounslow Recycling Hub
+              </div>
             </div>
-            <div className="navbarItemBox">
+            <div className={style["navbarItemBox"]}>
               {navList.map((v, i) => {
                 return (
-                  <a href={`#${v.target}`} key={i} className="navbarItem">
+                  <a
+                    href={`#${v.target}`}
+                    key={i}
+                    className={style["navbarItem"]}
+                  >
                     {v.label}
                   </a>
                 );
@@ -37,7 +46,7 @@ export default function Navbar() {
           </div>
         </div>
       </div>
-      <div className="fixedHeight"></div>
+      <div className={style["fixedHeight"]}></div>
     </div>
   );
 }
